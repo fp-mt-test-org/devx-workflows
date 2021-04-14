d0f07772182603929157977c205b778f8432ac71
Matthew Thomas <matthewthomas@flexport.com>
2021-04-14 16:34:22 -0700

    Fix var

--- scripts/configure-localhost.sh
+++ scripts/configure-localhost.sh
@@ -16,7 +16,7 @@ if ! grep -q "${flex_alias}" "${profile_path}"; then
     # Save it to the profile so it's execute for each shell session.
     echo -e "${flex_alias}\n${profile_content}" > "${profile_path}"
 
-    echo "Added ${flex_relative_path} to your ${profile_path}."
+    echo "Added ${flex_alias} to your ${profile_path}."
 
     # Start a new shell session so that the
     # user can use the alias immediately.
