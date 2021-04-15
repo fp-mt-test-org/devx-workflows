7b2bdfc004c730d49bbd078e9b1f746af3fb7886
Matthew Thomas <matthewthomas@flexport.com>
2021-04-14 20:10:30 -0700

    feat: adds tagging support

--- scripts/build-flex.sh
+++ scripts/build-flex.sh
@@ -4,9 +4,29 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+# Tag if in CI and on main branch
+trunk_branch=$(git branch | grep -o -m1 "\b\(master\|main\)\b")
+current_branch=$(git branch --show-current)
+
+echo "Trunk Branch: ${trunk_branch}"
+echo "Current Branch: ${current_branch}"
+
+if [[ -n "${CI:-}" ]]; then
+    echo "Detected running in CI, checking braches for tagging..."
+
+    if [[ "${trunk_branch}" == ${current_branch} ]]; then
+        brew install caarlos0/tap/svu
+        git tag $(svu n)
+        git push --tags
+    fi
+fi
+
 devx_workflow_scripts_folder=".devx-workflows"
 os="darwin" && [[ -n "${CI:-}" ]] && os="linux"
 
 # Build and output the workflow binaries.
 goreleaser --snapshot --skip-publish --rm-dist
-mkdir -p ${devx_workflow_scripts_folder} && tar -xzvf dist/$(ls dist | grep -e "SNAPSHOT.*${os}") -C ${devx_workflow_scripts_folder}
+
+mkdir -p ${devx_workflow_scripts_folder}
+tar -xzvf dist/$(ls dist | grep -e "SNAPSHOT.*${os}") -C ${devx_workflow_scripts_folder}
+
