d0ca260587a5349bb32947e110beb1e34619f37b
Matthew Thomas <matthewthomas@flexport.com>
2021-04-15 16:06:21 -0700

    fix: Gets the install-flex working after download from GH.

--- scripts/user/install-flex.sh
+++ scripts/user/install-flex.sh
@@ -10,19 +10,21 @@ install_dir='.devx-workflows'
 os=$(uname | tr '[:upper:]' '[:lower:]')
 file_name="flex_${os}_amd64.tar.gz"
 url="https://github.com/fp-mt-test-org/devx-workflows/releases/latest/download/${file_name}"
+download_file_path="${download_dir}/${file_name}"
 
 mkdir -p "${install_dir}"
 mkdir -p "${download_dir}"
 
-cd "${download_dir}"
-
 if [ "$skip_download" == "false" ]; then
     echo "Downloading ${url} to ${download_dir}"
-    curl -L "${url}"
+    curl -L "${url}" --output "${download_file_path}"
 fi
 
-echo "Extracting ${file_name} to ${install_dir}"
-tar -xf "${file_name}" -C "../${install_dir}"
+echo "Extracting ${download_file_path} to ${install_dir}"
+tar -xf "${download_file_path}" -C "${install_dir}"
+
+echo "Cleaning up ${download_dir}"
+rm -fdr "${download_dir}"
 
 echo "Installation complete!"
 echo ""
