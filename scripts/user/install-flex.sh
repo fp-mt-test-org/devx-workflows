d8134843de1f7e232e9cd29463c6af71706fe048
Matthew Thomas <matthewthomas@flexport.com>
2021-04-15 15:12:20 -0700

    fix: Improves the installer and removes the old binaries.

--- scripts/user/install-flex.sh
+++ scripts/user/install-flex.sh
@@ -4,10 +4,25 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-target_dir='.devx-workflows'
+skip_download="${skip_download:=false}"
+download_dir='dist'
+install_dir='.devx-workflows'
 os=$(uname | tr '[:upper:]' '[:lower:]')
-url="https://github.com/fp-mt-test-org/devx-workflows/releases/latest/download/flex_${os}_amd64.tar.gz"
+file_name="flex_${os}_amd64.tar.gz"
+url="https://github.com/fp-mt-test-org/devx-workflows/releases/latest/download/${file_name}"
 
-echo "Fetching ${url}"
+mkdir -p "${install_dir}"
+mkdir -p "${download_dir}"
 
-mkdir -p "${target_dir}" && curl -LX GET "${url}" | tar -xvz -C "${target_dir}"
+cd "${download_dir}"
+
+if [ "$skip_download" == "false" ]; then
+    echo "Downloading ${url} to ${download_dir}"
+    curl -L "${url}"
+fi
+
+echo "Extracting ${file_name} to ${install_dir}"
+tar -xf "${file_name}" -C "../${install_dir}"
+
+echo "Installation complete!"
+echo ""
