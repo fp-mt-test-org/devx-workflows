038f08b8ff54b7a3e08c28af4f10b28b52f6fac1
Matthew Thomas <matthewthomas@flexport.com>
2021-04-15 15:53:47 -0700

    fix: Clean up lint warnings.

--- scripts/test-flex.sh
+++ scripts/test-flex.sh
@@ -8,6 +8,7 @@ devx_scripts_dir=".devx-workflows"
 helloworld_dir="devx-workflows-test-empty-repo"
 build_cmd="hello"
 
+# Clear out the test repo if left over from previous test.
 trap "rm -rf ${helloworld_dir} ../${helloworld_dir}" ERR EXIT
 
 echo "--- Cloning helloworld service repository"
@@ -16,10 +17,10 @@ cp -R ${devx_scripts_dir} ${helloworld_dir}
 cd ${helloworld_dir}
 
 echo "--- Testing init script"
-{ echo "echo hello"; sleep 1; echo "done"; sleep 1; echo "helloworld-service"; } | ./${devx_scripts_dir}/flex init
+{ echo "echo ${build_cmd}"; sleep 1; echo "done"; sleep 1; echo "helloworld-service"; } | ./${devx_scripts_dir}/flex init
 
 echo "--- Running build command"
-if [ $(./${devx_scripts_dir}/flex build) != "hello" ]; then
+if [ "$(./${devx_scripts_dir}/flex build)" != "${build_cmd}" ]; then
     echo "Build command was not initialized correctly!"
     exit 1
 fi
