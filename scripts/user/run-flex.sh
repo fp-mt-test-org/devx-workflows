5500c3218bc4f5edab6f5c73eb6721dac7119105
Matthew Thomas <matthewthomas@flexport.com>
2021-05-05 16:23:12 -0700

    fix: Fixes run-flex to not fail on first run when the service_config.yml does not yet exist.

--- scripts/user/run-flex.sh
+++ scripts/user/run-flex.sh
@@ -8,24 +8,28 @@ devx_workflows_path='./.devx-workflows'
 flex_path="${devx_workflows_path}/flex"
 flex_version_command="${flex_path} -version"
 initial_flex_version=$(${flex_version_command})
+service_config_path='./service_config.yml'
 
 echo "Running Flex ${initial_flex_version}"
 
-service_config=$(cat ./service_config.yml)
+# Check the service_config, if it exists (i.e. is not first run of flex)
+if [ -f "${service_config_path}" ]; then
+    service_config=$(cat ${service_config_path})
 
-if [[ "${service_config}" =~ [0-9]+.[0-9]+.[0-9]+ ]]; then
-    configured_flex_version="${BASH_REMATCH[0]}"
-    echo "service_config: flex: version: ${configured_flex_version}"
+    if [[ "${service_config}" =~ [0-9]+.[0-9]+.[0-9]+ ]]; then
+        configured_flex_version="${BASH_REMATCH[0]}"
+        echo "service_config: flex: version: ${configured_flex_version}"
 
-    # Regex for matching snapshot versions such as v0.8.3-SNAPSHOT-27afad4
-    configured_flex_version_regex="v${configured_flex_version}.*"
+        # Regex for matching snapshot versions such as v0.8.3-SNAPSHOT-27afad4
+        configured_flex_version_regex="v${configured_flex_version}.*"
 
-    if ! [[ "${initial_flex_version}" =~  ${configured_flex_version_regex} ]]; then
-        echo "Current version is different than configured, upgrading..."
-        "${devx_workflows_path}/scripts/user/install-flex.sh" "${configured_flex_version}"
-        echo "Current version is now:"
-        ${flex_version_command}
-        echo "Upgrade complete."
+        if ! [[ "${initial_flex_version}" =~  ${configured_flex_version_regex} ]]; then
+            echo "Current version is different than configured, upgrading..."
+            "${devx_workflows_path}/scripts/user/install-flex.sh" "${configured_flex_version}"
+            echo "Current version is now:"
+            ${flex_version_command}
+            echo "Upgrade complete."
+        fi
     fi
 fi
 
